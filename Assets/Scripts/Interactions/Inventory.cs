8b7e759bd7295dcc95924fa2928985c4b001ace5
Reuben Chavez <rchavez0215@gmail.com>
2023-02-16 17:05:38 -0800

    Added simple inventory and items

--- Assets/Scripts/Interactions/Inventory.cs
+++ Assets/Scripts/Interactions/Inventory.cs
@@ -1,8 +1,54 @@
-using System;
-using System.Generics;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour 
+public class Inventory : MonoBehaviour
 {
-    [SerializeField] private Dictionary<int, string>  _inventory;
+    // Dictionary to hold the items in the inventory
+    private Dictionary<string, int> items = new Dictionary<string, int>();
+
+    // Add an item to the inventory
+    public void AddItem(string itemName, int amount)
+    {
+        if (items.ContainsKey(itemName))
+        {
+            items[itemName] += amount;
+        }
+        else
+        {
+            items[itemName] = amount;
+        }
+    }
+
+    // Remove an item from the inventory
+    public void RemoveItem(string itemName, int amount)
+    {
+        if (items.ContainsKey(itemName))
+        {
+            items[itemName] -= amount;
+            if (items[itemName] < 0)
+            {
+                items[itemName] = 0;
+            }
+        }
+    }
+
+    // Check if the inventory contains an item
+    public bool HasItem(string itemName)
+    {
+        return items.ContainsKey(itemName);
+    }
+
+    // Get the number of a particular item in the inventory
+    public int GetItemAmount(string itemName)
+    {
+        if (items.ContainsKey(itemName))
+        {
+            return items[itemName];
+        }
+        else
+        {
+            return 0;
+        }
+    }
 }
