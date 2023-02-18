a5490ef24c0e7653c56a1c3022104885c482d650
Steven Ren <stevenren999@gmail.com>
2023-02-18 15:52:13 -0800

    added a basic script for stats

---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //when equipment or item is changed, change stats TODO
+    }
+
+}
