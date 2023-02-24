89fd89055bbd41627586cb11500d49f6ab3a77fc
Reuben Chavez <rchavez0215@gmail.com>
2023-02-23 17:27:30 -0800

    Almost Finished with interaction system

--- Assets/Scripts/Interactions/Interaction_Ui.cs
+++ Assets/Scripts/Interactions/Interaction_Ui.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class Interaction_Ui : MonoBehaviour
 {
     private Camera _main;
-
+    [SerializeField] private GameObject _uiPanel;
+    [SerializeField] private TextMeshProUGUI _promtText;
     private void Start()
     {
         _main = Camera.main;
+        _uiPanel.SetActive(false);
         
     }
 
@@ -17,5 +20,19 @@ public class Interaction_Ui : MonoBehaviour
     {
         var rotation = _main.transform.rotation;
         transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+    }
+
+    public bool IsDisplayed = false;
+    public void SetUp(string promtText) 
+    {
+        _promtText.text = promtText;
+        _uiPanel.SetActive(true);
+        IsDisplayed = true;
+    }
+
+    public void Close() 
+    {
+        _uiPanel.SetActive(false);
+        IsDisplayed = false;
     }
 }
