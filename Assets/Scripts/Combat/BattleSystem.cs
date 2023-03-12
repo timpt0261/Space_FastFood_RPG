56cb0530604f8f19b77dd84a800fe64c31c49f81
Reuben Chavez <rchavez0215@gmail.com>
2023-03-11 18:06:13 -0800

    Worked on enemy ai

--- Assets/Scripts/Combat/BattleSystem.cs
+++ Assets/Scripts/Combat/BattleSystem.cs
@@ -7,8 +7,39 @@ public class BattleSystem : MonoBehaviour
 {
     public BattleState state;
 
+    // Player 
+    [SerializeField]
+    private Transform player_postion;
+    [SerializeField]
+    private GameObject player;
+
+
+    [SerializeField]
+    private Transform[] enemy_postion;
+    [SerializeField]
+    private GameObject[] enemies;
+
+    private void Awake()
+    {
+       
+    }
+
     void Start() 
     {
         state = BattleState.START;
+
+        player.transform.position = player_postion.position;
+
+        // Check that the arrays are the same length
+        if (enemies.Length != enemy_postion.Length)
+        {
+            Debug.LogError("Arrays must be the same length!");
+            return;
+        }
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].transform.position = enemy_postion[i].position;
+        }
     }
 }
