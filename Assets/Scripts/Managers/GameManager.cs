b05d53c13f07e0824d81e3674541935b5dfa2f1e
Reuben Chavez <rchavez0215@gmail.com>
2023-04-20 01:27:12 -0700

    Working on Game manger

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -30,28 +30,15 @@ public class GameManager : MonoBehaviour
         {
             case GameState.DEFUALT:
                 break;
-            case GameState.DIALOUGE:
-                HandelDialouge();
-                break;
             case GameState.EXPLORATION:
-                HandelExploration();
                 break;
             case GameState.ENTER_COMBAT:
-                HandelCombat();
-                break;
-            case GameState.LEAVING_COMBAT:
-                break;
-            case GameState.INVENTORY:
-                break;
-            case GameState.INTERACTION:
-                break;
-            case GameState.ENTER_SERVING:
                 break;
             case GameState.PLAYER_TURN:
                 break;
             case GameState.ENEMY_TURN:
                 break;
-            case GameState.LEAVING_SERVING:
+            case GameState.SERVING:
                 break;
             case GameState.GAME_OVER:
                 break;
@@ -60,46 +47,24 @@ public class GameManager : MonoBehaviour
             case GameState.LOBBY:
                 break;
             case GameState.MENU:
+                // Refense to script
                 break;
             case GameState.OPTIONS:
                 break;
-           
         }
-
         OnGameStateChanged?.Invoke(newGameState);
     }
 
-    private void HandelCombat()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void HandelExploration()
-    {
-        // 
-    }
-
-    private void HandelDialouge()
-    {
-        // Pause all enemies
-        // Pull up Menu 
-        // Read script
-    }
 }
 
 public enum GameState
 {
     DEFUALT,
-    DIALOUGE, // When ever player is speaking with NPC
     EXPLORATION, // When ever the player is outside the resturant
     ENTER_COMBAT, // When the Player is now fighting an Enemy (Movment should be turned off)
     PLAYER_TURN,
     ENEMY_TURN,
-    LEAVING_COMBAT, // Update level progression of the Player and Invetory
-    INVENTORY, // When the Player Pulls up thier inventort
-    INTERACTION, // When Player sees Door, Chest, or Plant
-    ENTER_SERVING, // Player has now playing mini-game
-    LEAVING_SERVING, // Update Money Gained, Level Progression, Possible Resturna Upgrades
+    SERVING, // Player has now playing mini-game
     GAME_OVER, // Should Play when the player has been killed
     GAME_START, // Should resume Gameplay from last save point
 
---
+++ Assets/Scripts/Player/PlayerToWorldManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerToWorldManager : MonoBehaviour
+{
+    // This script should change the Player respond to the WorldManger
+
+    private PlayerInput input;
+    private void Awake()
+    {
+        GameManager.OnGameStateChanged += IsExploring;
+
+        input = GetComponent<PlayerInput>();
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnGameStateChanged -= IsExploring;
+    }
+
+
+    // The Following Script Disable the player's input when not exploring or in th lobby
+    private void IsExploring(GameState state)
+    {
+        if (state != GameState.EXPLORATION && state != GameState.LOBBY)
+        {
+            input.actions.Disable();
+        }
+        else 
+        {
+            input.actions.Enable();
+        }
+        
+    }
+    
+}
